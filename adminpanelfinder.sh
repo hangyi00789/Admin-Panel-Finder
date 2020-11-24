2ffe2c21ee45c19dfdaa21aa86a10760a0c124cc
hangyi00789 <73415433+hangyi00789@users.noreply.github.com>
2020-11-24 18:15:47 +0630

    Update adminpanelfinder.sh

--- adminpanelfinder.sh
+++ adminpanelfinder.sh
@@ -44,16 +44,17 @@ echo ""
 echo -e "\e[1;33m Coder    : \e[1;35m Z847HG,RH4P50DY \e[0m"
 echo -e '\e[1;33m Page link: \e[1;35m https://www.facebook.com/BurmeseDemomForce/ \e[0m'
 echo ""
+http="HTTP/1.1 200 ok"
 read -p $'\e[1;31m Target site==> \e[0m' site
-result1=$(curl -IKs $site)
+result1=$(curl -IKs $site | grep "HTTP/")
 echo $result1
-res1=$(echo "$result1")
+res1=$(echo "$result1" | grep -o '[200]\+')
 while IFS= read -r path
 do
 
-result=$(curl -IKs $site/$path)
+result=$(curl -IKs $site/$path | grep "HTTP/")
 echo $result
-res=$(echo "$result")
+res=$(echo "$result" | grep -o '[200]\+')
 echo -e "\e[1;35m Scanning site:: $site/$path \e[0m"
 if [[ $res1 == $res ]];then
 echo -e "\e[1;35m Admin Panel Found:: $site/$path \e[0m"
